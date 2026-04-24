6ac847bfc8aa8e4a339aa7f3967f411680cfc258
Iair1 <166546999+Iair1@users.noreply.github.com>
2026-04-24 15:48:39 -0300

    Create debconfig.js

---
+++ debconfig.js
@@ -0,0 +1,9 @@
+const config = {
+    host: 'ep-quiet-butterfly-acwocpa2-pooler.sa-east-1.aws.neon.tech',
+    database: 'neondb',
+    user: 'neondb_owner',
+    password: 'npg_2ZvgXq8kbaLf',
+    post: 5432,
+    ssl: true
+}
+export default config;
