54666d1ec81827811e9fdc6cdb78ec77c86b76f1
Iair1 <166546999+Iair1@users.noreply.github.com>
2026-05-29 15:22:46 -0300

    ewd

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-//import "dotenv/config";
+import "dotenv/config";
 
 import UsuariosRouter from "../back/routers/usuarios.router.js";
 //import PeliculasRouter from "../back/routers/peliculas.router.js";
