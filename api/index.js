fc569587f2aa353cf265b41fedef0b20aab48d07
Iair1 <166546999+Iair1@users.noreply.github.com>
2026-05-31 15:14:42 -0300

    api

--- api/index.js
+++ api/index.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (__, res) => res.send("Bienvenido a la API de Películas"));
+app.get("/api", (__, res) => res.send("Bienvenido a la API de Películas"));
 
-app.use("/usuarios", UsuariosRouter);
+app.use("/api/usuarios", UsuariosRouter);
 
 // Este export es el que Vercel necesita
 export default app;
