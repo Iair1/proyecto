62e55fcd9126c3d925e6a5238a060544fcf6ac9e
Iair1 <166546999+Iair1@users.noreply.github.com>
2026-06-27 19:13:56 -0300

    jn

---
+++ vitest.config.js
@@ -0,0 +1,9 @@
+// vitest.config.js
+import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => ({
+  test: {
+    env: loadEnv(mode, process.cwd(), ""),
+  },
+}));
